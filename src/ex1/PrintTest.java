d190330d0f8dbefaebfaf2aeb36abc2c6ab35ab7
KOGUMA <smallbear0608@gmail.com>
2018-04-10 16:59:21 +0900

    ...

--- src/ex1/PrintTest.java
+++ src/ex1/PrintTest.java
@@ -3,5 +3,7 @@ package ex1;
 public class PrintTest {
 	public static void main(String[] args) {
 		System.out.print("TEST");
+		System.out.println("Data");
+		System.out.println("Upload");
 	      }
 }
